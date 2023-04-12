1168bdf61fec1b67b8f9a40efdd7f85a75745938
shirzada0725 <shirzada0725@gmail.com>
2023-04-11 19:52:42 -0500

    second commit

--- src/gitclass/Egit1.java
+++ src/gitclass/Egit1.java
@@ -2,6 +2,7 @@ package gitclass;
 
 public class Egit1 {
     public static void main(String[] args) {
-        
+        System.out.println( " i am learning GIT");
+        System.out.println( "good job");
     }
 }
